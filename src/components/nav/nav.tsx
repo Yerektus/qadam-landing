8cee8b588a86e34c2fc2bc7163121a8420d9cbd6
Yerektus <qop6261@gmail.com>
2026-04-22 18:21:26 +0500

    feat: add link to dashboard

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -1,11 +1,14 @@
-import { Button } from "../ui/button";
+import { Button, buttonVariants } from "../ui/button";
 
+const appUrl = import.meta.env.PUBLIC_APP_URL
 
 const Nav = () => {
     return (
         <nav className="fixed z-50 w-full h-14 flex justify-between items-center px-8 bg-white">
             <div className="font-heading text-xl">QADAM</div>
-            <Button variant={"default"} className="px-5">Вход</Button>
+            <a href={appUrl} className={buttonVariants({ className: "px-5" })}>
+                Вход
+            </a>
         </nav>
     )
 }
