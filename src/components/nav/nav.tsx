9e33a413417e9c9be12b88df76d578870dfeb4d3
Yerektus <qop6261@gmail.com>
2026-04-23 13:04:11 +0500

    feat: update nav

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -4,10 +4,10 @@ const appUrl = import.meta.env.PUBLIC_APP_URL
 
 const Nav = () => {
     return (
-        <nav className="fixed z-50 w-full h-14 flex justify-between items-center px-8 bg-white">
+        <nav className="fixed z-50 shadow-xs w-full h-12 flex justify-between items-center px-8 bg-white">
             <div className="font-heading text-xl">QADAM</div>
-            <a href={appUrl} className={buttonVariants({ className: "px-5" })}>
-                Вход
+            <a href={appUrl} className={buttonVariants({ size: 'sm' })}>
+                Войти
             </a>
         </nav>
     )
